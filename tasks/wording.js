fa36ddb7f4a64370b0207f1267db1ef3cf5c3897
pxceccaldi <pxceccaldi@af83.com>
2013-03-08 14:25:57 +0100

    add mutualizing feature

--- tasks/wording.js
+++ tasks/wording.js
@@ -16,7 +16,11 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('wording', 'Create your wording translations file', function() {
 
     // Merge task-specific and/or target-specific options with these defaults.
+
+    // Set delimiters for your keys
     var delimiters = /\{\%=(.+?)\%\}/g;
+    var mutualizer = "mutual";
+
     var options = this.options({
       punctuation: '.',
       separator: ', '
@@ -26,11 +30,14 @@ module.exports = function(grunt) {
     function template(filepath) {
 
       var content = grunt.file.read(filepath);
+
+      // Get file keys and push them in a table
       var result = [], a;
       while ((a = delimiters.exec(content))) {
         result.push(a[1].trim());
       }
 
+      // Put each file of the file path in a table
       var path = require('path');
       var getExtension = path.extname(filepath);
       var removeExtension = filepath.replace(getExtension, '');
@@ -39,6 +46,8 @@ module.exports = function(grunt) {
       var builder = JSON.parse(grunt.file.read('test/wording.json'));
 
       var data = builder;
+      data.mutual = data.mutual || {};
+
       // Build nested object using the filepath until filename
       for (var i = 0; i < splitPath.length; i++ ) {
         builder[splitPath[i]] = builder[splitPath[i]] || {};
@@ -46,15 +55,36 @@ module.exports = function(grunt) {
       }
 
       // Build file object with keys
+      var mutuals = [];
+      var _mutualizer = mutualizer + '.';
+      console.log(mutualizer);
+      for ( i = 0; i < result.length; i++) {
+        if (result[i].indexOf(_mutualizer) === 0 ) {
+
+          var getMutual = result.splice(i, 1);
+          var cutMutual = getMutual[0].replace(_mutualizer, '');
+          console.log(cutMutual);
+
+          mutuals.push(cutMutual);
+
+        }
+      }
       for ( i = 0; i < result.length; i++) {
         builder[result[i]] = builder[result[i]] || '';
+      }
+
+      var templateData = grunt.util._.clone(builder);
+      templateData.mutual = data.mutual;
+
+      for ( i = 0; i < mutuals.length; i++) {
+        data.mutual[mutuals[i]] = data.mutual[mutuals[i]] || '';
       }
 
       // Create and fill wording.json
       grunt.file.write('test/wording.json', JSON.stringify(data, null, 2));
 
       // Replace keys in templates with their associated wordings
-      grunt.util._.template(content, builder, {interpolate: delimiters});
+      grunt.util._.template(content, templateData, {interpolate: delimiters});
     }
 
     // Iterate over all specified file groups.
